f26666fdc3ac077c8bbed5990194a7888f73e48f
Tim Kravel <TKravel@Yahoo.com>
2022-01-19 01:43:12 -0500

    Fetch route set up to get / send movie data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const PORT = 3001;
+const fetch = require('node-fetch');
 
 const app = express();
 
@@ -53,19 +54,33 @@ const getGenreCode = (reqGenres) => {
 			}
 		}
 	}
-	if (genreList.length > 0) {
+	if (genreList.length > 1) {
 		genreList = genreList.slice(0, -1);
 	}
 	return genreList;
 };
 
 app.post('/', (req, res) => {
-	console.log(req.body);
 	const provider = req.body.provider;
 	const startDate = req.body.startDate;
 	const endDate = req.body.endDate;
-	// const rating = req.body.rating;
 	const genres = getGenreCode(req.body.genre);
+
+	fetch(
+		`https://streaming-availability.p.rapidapi.com/search/pro?country=us&service=${provider}&type=movie&order_by=year&year_min=${startDate}&year_max=${endDate}&genre=${genres}&page=1&desc=true&output_language=en&language=en`,
+		{
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
+				'x-rapidapi-key': process.env.API_KEY,
+				'content-type': 'application/json',
+			},
+		}
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			res.json({ docs: data.results, count: data.total_pages });
+		});
 });
 
 app.listen(PORT, () => {
